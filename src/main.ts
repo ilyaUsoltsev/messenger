b9f59e0418fb301c05ac87d0ccaf3c8b6443bab8
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2025-09-28 15:46:57 +0200

    Add login page

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+import Handlebars from 'handlebars';
+import * as Components from './components';
+import * as Pages from './pages';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const pages = {
+  login: [Pages.LoginPage],
+  nav: [Pages.NavigatePage],
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+Object.entries(Components).forEach(([name, template]) => {
+  Handlebars.registerPartial(name, template);
+});
+
+function navigate(page: string) {
+  //@ts-ignore
+  const [source, context] = pages[page];
+  const container = document.getElementById('app')!;
+
+  const temlpatingFunction = Handlebars.compile(source);
+  console.log('html', temlpatingFunction(context));
+  container.innerHTML = temlpatingFunction(context);
+}
+
+document.addEventListener('DOMContentLoaded', () => navigate('login'));
+
+document.addEventListener('click', (e) => {
+  //@ts-ignore
+  const page = e.target.getAttribute('page');
+  if (page) {
+    navigate(page);
+
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+});
